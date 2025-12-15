a040441b32a052d1cd2c98491260bf206f755eeb
CASPY <kifimetro@gmail.com>
2025-12-15 22:03:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,25 +17,3 @@ tabs.forEach(tab => {
         // Тут можно фильтровать товары по категории
     });
 });
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const buttons = document.querySelectorAll('.tab-btn');
-    const contents = document.querySelectorAll('.tab-content');
-
-    buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-
-            // убираем active у всех
-            buttons.forEach(b => b.classList.remove('active'));
-            contents.forEach(c => c.classList.remove('active'));
-
-            // добавляем active нужным
-            btn.classList.add('active');
-            document.getElementById(btn.dataset.tab).classList.add('active');
-        });
-    });
-
-});
